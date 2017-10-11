e9ef509d59b3c94df63bf9e2c92eaaaf5a682a16
skysider <junchaoluan@gmail.com>
2017-10-11 14:39:08 +0800

    update bind dockerfile

--- bind/update-bind.sh
+++ bind/update-bind.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-rm -rf /work/dist/
+rm -rf /work/dist-master/
 cd bind9
 git pull origin
 CC=hfuzz-clang CXX=hfuzz-clang++ \
 CFLAGS="-fsanitize=address -Wno-logical-not-parentheses -Wno-shift-negative-value -Wno-logical-not-parentheses -g -ggdb -O3" \
-./configure --prefix=/work/dist/ --disable-threads \
+./configure --prefix=/work/dist-master/ --disable-threads \
 --without-gssapi --disable-chroot --disable-linux-caps --disable-seccomp \
 --without-libtool --enable-ipv6 --enable-atomic --enable-epoll --with-openssl=no
 make clean
 make -j$(nproc)
 make install
 cd ..
-cp honggfuzz/examples/bind/named.conf dist/etc/
-cp honggfuzz/examples/bind/test.zone dist/etc/
-sed -i 's/home\/jagger\/fuzz\/bind/work/g' dist/etc/named.conf
+cp honggfuzz/examples/bind/named.conf dist-master/etc/
+cp honggfuzz/examples/bind/test.zone dist-master/etc/
+sed -i 's/home\/jagger\/fuzz\/bind/work/g' dist-master/etc/named.conf
